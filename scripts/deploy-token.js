96bd0baf0c5583f5d2ba86a866fe7d3cf173f402
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-07-19 12:28:57 +0200

    add deployment scripts

---
+++ scripts/deploy-token.js
@@ -0,0 +1,27 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const name = "Annotoken"
+  const symbol ="ANN"
+
+  const token = await hre.ethers.deployContract("Token", [name, symbol]);
+
+  await token.waitForDeployment();
+
+  console.log(
+    `Token ${name} [${symbol}] deployed to ${token.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
